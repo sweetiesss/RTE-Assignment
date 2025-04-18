614e0d20e43355be0c53342fd1d2825aff6afc25
sweetiesss <95135375+sweetiesss@users.noreply.github.com>
2025-04-18 14:35:02 +0700

    finish register feature

--- Api Sample/src/main/java/com/example/apisample/service/Interface/UserService.java
+++ Api Sample/src/main/java/com/example/apisample/service/Interface/UserService.java
@@ -2,13 +2,16 @@ package com.example.apisample.service.Interface;
 
 
 import com.example.apisample.entity.User;
+import com.example.apisample.exception.emailservice.EmailCannotBeSendException;
 import com.example.apisample.exception.jwtservice.InvalidCredentialsException;
+import com.example.apisample.exception.jwtservice.RoleDoesNotExistException;
 import com.example.apisample.exception.otpservice.InvalidOtpCodeException;
 import com.example.apisample.exception.otpservice.OtpDoesNotExistException;
 import com.example.apisample.exception.otpservice.OtpExpiredException;
 import com.example.apisample.exception.otpservice.OtpHasBeenUsedException;
 import com.example.apisample.exception.userservice.*;
-import com.example.apisample.model.dto.authdto.ResetPasswordRequestDTO;
+import com.example.apisample.model.dto.auth.ResetPasswordRequestDTO;
+import com.example.apisample.model.dto.user.UserRegisterRequestDTO;
 
 public interface UserService {
 
@@ -17,4 +20,5 @@ public interface UserService {
     void saveUser(User user);
     void logout(User user) throws UserDoesNotLoginException;
     void resetPassword(ResetPasswordRequestDTO dto, User user) throws OtpDoesNotExistException, InvalidOtpCodeException, OtpHasBeenUsedException, OtpExpiredException;
+    void register(UserRegisterRequestDTO dto) throws UserAlreadyExistsException, RoleDoesNotExistException, EmailCannotBeSendException;
 }
